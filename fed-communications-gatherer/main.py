90ad2cd1a2ae9d1923df7d3200e5e27cbb3f9e24
Karan Patel <karanpatel4567@gmail.com>
2021-03-07 12:51:48 -0800

    Updating main

--- fed-communications-gatherer/main.py
+++ fed-communications-gatherer/main.py
@@ -2,6 +2,7 @@ import logging
 
 from src.definitions import POLICY_STATEMENTS_OUTPUT_DIR
 from src.fomc.FOMCCommunicationDocsService import FOMCCommunicationDocsService
+from src.fomc.client.domain.FOMCDocType import FOMCDocType
 
 logging.basicConfig(format="'%(asctime)s' %(name)s : %(message)s'", level=logging.INFO)
 logger = logging.getLogger("main")
@@ -9,7 +10,7 @@ logger = logging.getLogger("main")
 if __name__ == "__main__":
     fomc_communication_docs_service = FOMCCommunicationDocsService()
 
-    # fomc_communication_docs_service.export_fomc_docs(
-    #     FOMCDocType.POLICY_STATEMENTS, POLICY_STATEMENTS_OUTPUT_DIR
-    # )
-    fomc_docs = fomc_communication_docs_service.read_fomc_docs(POLICY_STATEMENTS_OUTPUT_DIR)
+    fomc_communication_docs_service.export_fomc_docs(
+        FOMCDocType.POLICY_STATEMENTS, POLICY_STATEMENTS_OUTPUT_DIR
+    )
+    # fomc_docs = fomc_communication_docs_service.read_fomc_docs(POLICY_STATEMENTS_OUTPUT_DIR)
