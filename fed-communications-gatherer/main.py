64bbc86c4285f9504a72a310362c2a0456f897bd
Lee Hoang <leehoang97@gmail.com>
2021-03-07 19:32:24 -0800

    Adding feature extraction code for word count and paragraph count

--- fed-communications-gatherer/main.py
+++ fed-communications-gatherer/main.py
@@ -23,6 +23,21 @@ def get_date_to_entity_sentiment(entity_name, entity_sentiment_result):
     return sorted(date_to_entity_sentiment, key=lambda item: item[0])
 
 
+# Feature extraction: Get number of paragraphs per speech.
+def get_number_of_paragraphs(docs):
+	for fomc_doc in docs:
+		print("Document")
+		print(len(fomc_doc.paragraphs))
+		print("\n")
+
+# Feature extraction: Get number of words per speech.
+def get_word_count(docs):
+	for fomc_doc in docs:
+		speech = " ".join(fomc_doc.paragraphs)
+		word_list = speech.split()
+		print(len(word_list))
+
+
 if __name__ == "__main__":
     fomc_communication_docs_service = FOMCCommunicationDocsService()
 
@@ -36,22 +51,25 @@ if __name__ == "__main__":
     # load FOMC docs from disk
     fomc_docs = fomc_communication_docs_service.read_fomc_docs(POLICY_STATEMENTS_OUTPUT_DIR)
 
-    # perform entity sentiment analysis
-    entity_sentiment_analyzer = EntitySentimentAnalyzer()
-    entity_sentiment_result = entity_sentiment_analyzer.perform_entity_sentiment_analysis(fomc_docs)
-    entity_sentiment_result = sorted(entity_sentiment_result, key=lambda item: item[0].meeting_date)
+    get_number_of_paragraphs(fomc_docs)
+    get_word_count(fomc_docs)
 
-    # get sentiment values for a given entity over time
+    # # perform entity sentiment analysis
+    # entity_sentiment_analyzer = EntitySentimentAnalyzer()
+    # entity_sentiment_result = entity_sentiment_analyzer.perform_entity_sentiment_analysis(fomc_docs)
+    # entity_sentiment_result = sorted(entity_sentiment_result, key=lambda item: item[0].meeting_date)
+
+    # # get sentiment values for a given entity over time
     # date_to_entity_sentiment = get_date_to_entity_sentiment('employment', entity_sentiment_result)
     # print(date_to_entity_sentiment)
 
-    # identify most common entities for which sentiment exists
-    entity_to_count_dict = dict()
-    for fomc_doc, entities in entity_sentiment_result:
-        entity_names_in_doc = {entity.name for entity in entities if entity.sentiment.score}
-        for entity_name in entity_names_in_doc:
-            if entity_name in entity_to_count_dict:
-                entity_to_count_dict[entity_name] = entity_to_count_dict[entity_name] + 1
-            else:
-                entity_to_count_dict[entity_name] = 1
-    pd.DataFrame.from_dict(entity_to_count_dict, orient='index').to_csv(os.path.join(OUTPUT_DIR, 'entity-count.csv'))
+    # # identify most common entities for which sentiment exists
+    # entity_to_count_dict = dict()
+    # for fomc_doc, entities in entity_sentiment_result:
+    #     entity_names_in_doc = {entity.name for entity in entities if entity.sentiment.score}
+    #     for entity_name in entity_names_in_doc:
+    #         if entity_name in entity_to_count_dict:
+    #             entity_to_count_dict[entity_name] = entity_to_count_dict[entity_name] + 1
+    #         else:
+    #             entity_to_count_dict[entity_name] = 1
+    # pd.DataFrame.from_dict(entity_to_count_dict, orient='index').to_csv(os.path.join(OUTPUT_DIR, 'entity-count.csv'))
