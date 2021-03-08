3c5c15fffd2036fa51f7efadb8f1c461b2ea1771
Karan Patel <karanpatel4567@gmail.com>
2021-03-07 19:26:24 -0800

    Export excel books containing entity sentiment over time

--- fed-communications-gatherer/main.py
+++ fed-communications-gatherer/main.py
@@ -14,22 +14,26 @@ logger = logging.getLogger("main")
 
 def get_date_to_entity_sentiment(entity_name, entity_sentiment_result):
     date_to_entity_sentiment = []
+    # TODO: aggregate lower and upper case entity names, aggregate by buckets
     for fomc_doc, entities in entity_sentiment_result:
         matching_entities = [entity for entity in entities if entity.name.lower() == entity_name and entity.sentiment.score]
         if matching_entities:
             if len(matching_entities) > 1:
                 logger.info("Found {} entity {} times in the same doc".format(entity_name, len(matching_entities)))
             date_to_entity_sentiment.append((fomc_doc.meeting_date, matching_entities[0].sentiment.score))
-    return sorted(date_to_entity_sentiment, key=lambda item: item[0])
+    date_to_entity_sentiment = sorted(date_to_entity_sentiment, key=lambda item: item[0])
+
+    pd.DataFrame.from_records(date_to_entity_sentiment).to_excel(os.path.join(OUTPUT_DIR, 'sentiment-for-{}-entity-overtime.xlsx'.format(entity_name)))
+    return date_to_entity_sentiment
 
 
 if __name__ == "__main__":
     fomc_communication_docs_service = FOMCCommunicationDocsService()
 
     # Note: only run this code once to download all the FOMC documents to your local disk
-    fomc_communication_docs_service.export_fomc_docs(
-        FOMCDocType.POLICY_STATEMENTS, POLICY_STATEMENTS_OUTPUT_DIR
-    )
+    # fomc_communication_docs_service.export_fomc_docs(
+    #     FOMCDocType.POLICY_STATEMENTS, POLICY_STATEMENTS_OUTPUT_DIR
+    # )
 
     # load FOMC docs from disk
     fomc_docs = fomc_communication_docs_service.read_fomc_docs(POLICY_STATEMENTS_OUTPUT_DIR)
@@ -40,8 +44,8 @@ if __name__ == "__main__":
     entity_sentiment_result = sorted(entity_sentiment_result, key=lambda item: item[0].meeting_date)
 
     # get sentiment values for a given entity over time
-    # date_to_entity_sentiment = get_date_to_entity_sentiment('employment', entity_sentiment_result)
-    # print(date_to_entity_sentiment)
+    date_to_entity_sentiment = get_date_to_entity_sentiment('inflation', entity_sentiment_result)
+    print(date_to_entity_sentiment)
 
     # identify most common entities for which sentiment exists
     entity_to_count_dict = dict()
